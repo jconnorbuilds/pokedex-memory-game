469988bd1c27fc067465742663590254850eae74
Joe Connor <joseph.connor.music@gmail.com>
2024-11-26 22:40:29 +0900

    Starts adding features to the inner lid

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -174,37 +174,34 @@ export default function App() {
         <Pokedex>
           <PokedexBody></PokedexBody>
           <PokedexLid>
-            <div className="lid__content">
-              <div className="choose-difficulty" onClick={handleLevelSelect}>
-                <h2>Difficulty</h2>
-                {LEVELS.map((level) => {
-                  const levelNameFormatted =
-                    level.charAt(0).toUpperCase() + level.slice(1);
+            <div className="choose-gen" onClick={handleGenerationSelect}>
+              <h2>Generations</h2>
+              {Array(NUM_OF_GENERATIONS)
+                .fill('')
+                .map((_, idx) => {
+                  const genNumber = idx + 1;
+                  const genSize = genSizes[genNumber];
                   return (
-                    <MenuButton key={level} value={level}>
-                      {levelNameFormatted}
-                    </MenuButton>
+                    <GenerationDisplay key={idx}>
+                      <MenuButton value={genNumber}>{genNumber}</MenuButton>
+                      <div className="gen-dex-completion">
+                        {genCompletion[genNumber] ? genCompletion[genNumber].length : 0} /{' '}
+                        {genSize ? genSize : '...'}
+                      </div>
+                    </GenerationDisplay>
                   );
                 })}
-              </div>
-              <div className="choose-gen" onClick={handleGenerationSelect}>
-                <h2>Generations</h2>
-                {Array(NUM_OF_GENERATIONS)
-                  .fill('')
-                  .map((_, idx) => {
-                    const genNumber = idx + 1;
-                    const genSize = genSizes[genNumber];
-                    return (
-                      <GenerationDisplay key={idx}>
-                        <MenuButton value={genNumber}>{genNumber}</MenuButton>
-                        <div className="gen-dex-completion">
-                          {genCompletion[genNumber] ? genCompletion[genNumber].length : 0}{' '}
-                          / {genSize ? genSize : '...'}
-                        </div>
-                      </GenerationDisplay>
-                    );
-                  })}
-              </div>
+            </div>
+            <div className="choose-difficulty" onClick={handleLevelSelect}>
+              <h2>Difficulty</h2>
+              {LEVELS.map((level) => {
+                const levelNameFormatted = level.charAt(0).toUpperCase() + level.slice(1);
+                return (
+                  <MenuButton key={level} value={level}>
+                    {levelNameFormatted}
+                  </MenuButton>
+                );
+              })}
             </div>
           </PokedexLid>
         </Pokedex>
--- src/components/PokedexLid.jsx
+++ src/components/PokedexLid.jsx
@@ -3,11 +3,14 @@ export default function PokedexLid({ children }) {
     <div className="pokedex__lid">
       <div className="lid__main">
         <div className="lid__main__inside face">
-          <div className="lid__main-content">{children}</div>
+          <div className="lid__main-content-wrapper">
+            <div className="lid__main-content">
+              <div className="lid__display"></div>
+              {children}
+            </div>
+          </div>
         </div>
-        {/* <div className="lid__main__left vert-edge"></div> */}
         <div className="lid__main__right vert-edge"></div>
-        {/* <div className="lid__main__top horiz-edge"></div> */}
         <div className="lid__main__top flat1 horiz-edge"></div>
         <div className="lid__main__top flat2 horiz-edge"></div>
         <div className="lid__main__top slant1 horiz-edge"></div>
