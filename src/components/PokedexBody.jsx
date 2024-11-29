ff3eac436b57687e3dcc7353f7f536cd995e8579
Joe Connor <joseph.connor.music@gmail.com>
2024-11-29 09:57:33 +0900

    Updates pokdex ratios and adds more buttons

--- src/components/PokedexBody.jsx
+++ src/components/PokedexBody.jsx
@@ -3,11 +3,35 @@ export default function PokedexBody({ children }) {
     <div className="pokedex__body">
       <div className="body__main">
         <div className="body__main__front face">
+          <div className="body__upper-overhang">
+            <div className="upper-overhang__front face">
+              <svg viewBox="0 0 112.5 112.5" xmlns="http://www.w3.org/2000/svg">
+                <g className="circle">
+                  <circle className="circle__outer" cx="56.25" cy="56.25" r="35"></circle>
+                  <circle className="circle__inner" cx="56.25" cy="56.25" r="30"></circle>
+                </g>
+              </svg>
+            </div>
+
+            <div className="upper-overhang__left vert-edge"></div>
+            <div className="upper-overhang__right vert-edge"></div>
+            <div className="upper-overhang__top horiz-edge"></div>
+            <div className="upper-overhang__bottom flat1 horiz-edge"></div>
+            <div className="upper-overhang__bottom flat2 horiz-edge"></div>
+            <div className="upper-overhang__bottom slant1 horiz-edge"></div>
+          </div>
           <div className="screen">
             <div className="screen-frame">
-              <div className="screen-inner"></div>
+              <div className="screen-inner">{children}</div>
             </div>
             <div className="screen-overlay"></div>
+          </div>
+          <div className="body__buttons">
+            <div className="buttons__dpad"></div>
+            <div className="buttons__ab">
+              <div className="ab ab__a"></div>
+              <div className="ab ab__b"></div>
+            </div>
           </div>
         </div>
         <div className="body__main__left vert-edge"></div>
@@ -15,23 +39,6 @@ export default function PokedexBody({ children }) {
         <div className="body__main__top horiz-edge"></div>
         <div className="body__main__bottom horiz-edge"></div>
         <div className="body__main__back face"></div>
-      </div>
-
-      <div className="body__upper-overhang">
-        <div className="upper-overhang__front face">
-          <svg viewBox="0 0 112.5 112.5" xmlns="http://www.w3.org/2000/svg">
-            <g className="circle">
-              <circle className="circle__outer" cx="56.25" cy="56.25" r="35"></circle>
-              <circle className="circle__inner" cx="56.25" cy="56.25" r="30"></circle>
-            </g>
-          </svg>
-        </div>
-        <div className="upper-overhang__left vert-edge"></div>
-        <div className="upper-overhang__right vert-edge"></div>
-        <div className="upper-overhang__top horiz-edge"></div>
-        <div className="upper-overhang__bottom flat1 horiz-edge"></div>
-        <div className="upper-overhang__bottom flat2 horiz-edge"></div>
-        <div className="upper-overhang__bottom slant1 horiz-edge"></div>
       </div>
     </div>
   );
