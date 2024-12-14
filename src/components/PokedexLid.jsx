16ee1072e1536a4fffe386b46e917c5d10682252
Joe Connor <joseph.connor.music@gmail.com>
2024-12-14 18:00:50 +0900

    Fixes some broken edges of the pokedex

--- src/components/PokedexLid.jsx
+++ src/components/PokedexLid.jsx
@@ -10,11 +10,12 @@ export default function PokedexLid({ children }) {
             </div>
           </div>
         </div>
-        <div className="lid__main__right vert-edge"></div>
+        <div className="lid__main__left vert-edge"></div>
         <div className="lid__main__top flat1 horiz-edge"></div>
         <div className="lid__main__top flat2 horiz-edge"></div>
         <div className="lid__main__top slant1 horiz-edge"></div>
         <div className="lid__main__bottom horiz-edge"></div>
+        <div className="lid__main__right vert-edge"></div>
         <div className="lid__main__outside face"></div>
       </div>
     </div>
