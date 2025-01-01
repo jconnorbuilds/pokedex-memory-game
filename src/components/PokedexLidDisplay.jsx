d56b92fe10f653fee5134548c52e894a1ecfb7aa
Joe Connor <joseph.connor.music@gmail.com>
2025-01-02 00:33:04 +0900

    Updates chart to use pokemon stat data

--- src/components/PokedexLidDisplay.jsx
+++ src/components/PokedexLidDisplay.jsx
@@ -3,38 +3,92 @@ import styles from '../styles/PokedexLidDisplay.module.css';
 import {
   Chart as ChartJS,
   RadialLinearScale,
-  ArcElement,
+  PointElement,
+  LineElement,
+  Filler,
   Tooltip,
   Legend,
 } from 'chart.js';
-import { PolarArea } from 'react-chartjs-2';
-ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
+import { Radar } from 'react-chartjs-2';
+
+ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend);
 
 export default function PokedexLidDisplay({ pokemon }) {
-  const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.5)',
-          'rgba(54, 162, 235, 0.5)',
-          'rgba(255, 206, 86, 0.5)',
-          'rgba(75, 192, 192, 0.5)',
-          'rgba(153, 102, 255, 0.5)',
-          'rgba(255, 159, 64, 0.5)',
+  console.log(pokemon);
+  const blankData = {};
+  const stats = pokemon?.data.stats;
+  const statNamesFormatted = stats?.map((stat) => {
+    const statNames = {
+      hp: 'HP',
+      attack: 'ATT.',
+      defense: 'DEF.',
+      'special-attack': 'SP. ATT.',
+      'special-defense': 'SP. DEF.',
+      speed: 'SPD.',
+    };
+    return statNames[stat.stat.name];
+  });
+  const data = pokemon
+    ? {
+        labels: statNamesFormatted,
+        datasets: [
+          {
+            label: pokemon?.name,
+            data: stats?.map((stat) => stat.base_stat),
+            lineColor: '#eee',
+            backgroundColor: 'rgba(75, 192, 192, 0.6)',
+            borderColor: 'rgba(75, 192, 192, 0.9)',
+            borderWidth: 2,
+            pointColor: 'rgba(153, 102, 255)',
+            pointHoverBorderColor: '#FFF',
+            pointHoverBorderWidth: 3,
+            pointBorderWidth: 0,
+            pointHoverRadius: 5,
+            pointStyle: 'rect',
+            pointHitRadius: 10,
+            pointSize: 4,
+            tension: 0.15,
+          },
         ],
-        borderWidth: 1,
+      }
+    : blankData;
+
+  const options = {
+    scales: {
+      r: {
+        beginAtZero: true,
+        angleLines: {
+          color: '#DDD',
+        },
+        grid: {
+          color: '#444',
+        },
+        pointLabels: {
+          display: true,
+          padding: 0,
+        },
+        suggestedMax: 200,
+        ticks: {
+          backdropColor: '#0006',
+          color: '#bbb',
+        },
       },
-    ],
+    },
+    plugins: {
+      legend: {
+        align: 'start',
+        position: 'chartArea',
+        // padding: 50,
+        // height: 50,
+        // display: false,
+      },
+    },
   };
 
   return (
     <>
       <div className={styles.lidDisplay}>
-        {pokemon && pokemon.name}
-        <PolarArea data={data}></PolarArea>
+        {pokemon ? <Radar data={data} options={options}></Radar> : null}
       </div>
     </>
   );
