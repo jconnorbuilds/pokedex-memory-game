33ca21d3c599fd802a6e0e33537c4e23db1aa8e3
Joe Connor <joseph.connor.music@gmail.com>
2025-02-23 11:12:34 +0900

    Removes unused files and adds basic README info

--- src/components/LoadingContent.jsx
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/LoadingContent.module.css';
-
-export default function LoadingContent({ isLoading, children }) {
-  return (
-    <div id="loading-screen" className={isLoading ? styles.shown : styles.hidden}>
-      {children}
-    </div>
-  );
-}
