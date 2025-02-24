dc4867c05d9045ae370960548a500a1e424b3072
Joe Connor <joseph.connor.music@gmail.com>
2025-02-24 15:29:45 +0900

    Removes unused code, adds error logging

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -58,21 +58,18 @@ export default function App() {
     signInWithPopup(auth, provider)
       .then((result) => createOrUpdateUserDbEntry(result.user))
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
         console.error(error); // TODO: Actually handle errors
       });
   };
 
-  // console.log(user, credential, token);
-
   const logUserOut = () => {
-    signOut(auth).then(() => {
-      // TODO: show logout UI?
-    });
+    signOut(auth)
+      .then(() => {
+        // TODO: show logout UI?
+      })
+      .catch((error) => {
+        console.error(error.code); // TODO: Actually handle errors
+      });
   };
 
   const allPokemonInGen = currentGenPkmnIds;
